1ff6a97ba16f99d6abe84b256cef4c22d80a84e9
trhuan9800 <tr.huan.8098@gmail.com>
2022-03-23 16:39:25 +0700

    add article like navbar footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 import { PetComponent } from './pet/pet.component';
-import {FormsModule} from "@angular/forms";
-import { ProductComponent } from './product/product.component';
-import { SimpleCaculatorComponent } from './simple-caculator/simple-caculator.component';
 
+import {FormsModule} from "@angular/forms";
+
+import { ProductComponent } from './product/product.component';
+
+import { SimpleCaculatorComponent } from './simple-caculator/simple-caculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
+import {ColorPickerModule} from "ngx-color-picker";
+import { ArticleComponent } from './article/article.component';
+import { LikeComponent } from './like/like.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeEditorComponent,
     PetComponent,
     ProductComponent,
-    SimpleCaculatorComponent
+    SimpleCaculatorComponent,
+    ColorPickerComponent,
+    ArticleComponent,
+    LikeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/color-picker/color-picker.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-color-picker',
+  templateUrl: './color-picker.component.html',
+  styleUrls: ['./color-picker.component.css']
+})
+export class ColorPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  color: string = '#2889e9'
+  arrayColors: any = {
+    color1: '#2883e9',
+    color2: '#e920e9',
+    color3: 'rgb(255,245,0)',
+    color4: 'rgb(236,64,64)',
+    color5: 'rgba(45,208,45,1)'
+  };
+  selectedColor: string = 'color1';
+}
